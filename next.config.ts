006c935a4bc950a5249ec3f79cb83fdab1332a26
newgirok <newgirok@gmail.com>
2026-08-30 17:10:32 +0900

    chore(init): Next.js 15 + TypeScript 프로젝트 기반 설정

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'mapbox-gl': 'mapbox-gl',
+      }
+    }
+    return config
+  },
+}
+
+export default nextConfig
